0c53a768ae5f0e036ca7ed0f762c0bcdeaddfa36
Matthew Chu <mchu341@gmail.com>
2025-02-03 21:18:16 -0500

    not found page

--- VS Code Projects/StickerSmash/app/_layout.tsx
+++ VS Code Projects/StickerSmash/app/_layout.tsx
@@ -2,8 +2,9 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-  <Stack>
-    <Stack.Screen name = "index" />
-  </Stack>
+    <Stack>
+      <Stack.Screen name="index" options={{ title: 'Home' }} />
+      <Stack.Screen name="about" options={{ title: 'About' }} />
+    </Stack>
   );
 }
